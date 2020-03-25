5f73060bbe2289923a0deb425bde22459a755e58
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-25 19:34:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ function  destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
+var newObj = Object.assign({}, obj);
 function deleteFromObjectByKey(object, key){
-  
+  delete newObj[key];
+  return newObj
+};
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
